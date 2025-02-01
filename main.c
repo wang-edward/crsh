086353a7143e843252ff3543d7f6d9ad02602389
Edward Wang <edawordwang@gmail.com>
2025-02-01 11:08:22 -0500

    try stuff

---
+++ main.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+#define MAX_ARGS 100
+
+int main(void) {
+    char *line = NULL;
+    size_t bufsize = 0;
+
+    while (1) {
+        printf("$ ");
+        fflush(stdout);
+
+        if (getline(&line, &bufsize, stdin) == -1) {
+            perror("getline");
+            break;
+        }
+
+        // remove trailing newlin
+        line[strcspn(line, "\n")] = '\0';
+
+        if (strlen(line) == 0) {
+            continue;
+        }
+
+        // parse
+        char *args[MAX_ARGS];
+        int arg_count = 0;
+        char *token = strtok(line, " ");
+        while (token != NULL && arg_count < MAX_ARGS - 1) {
+            args[arg_count++] = token;
+            token = strtok(NULL, " ");
+        }
+        args[arg_count] = NULL;
+
+        // handle "exit"
+        if (strcmp(args[0], "exit") == 0) {
+            break;
+        }
+
+        // fork
+        pid_t pid = fork();
+        if (pid < 0) {
+            perror("fork");
+            continue;
+        }
+
+        if (pid == 0) {
+            // within child process
+            if (execvp(args[0], args) == -1) {
+                perror("execvp");
+            }
+            exit(EXIT_FAILURE);
+        } else {
+            // within parent
+            int status;
+            if (waitpid(pid, &status, 0) == -1) {
+                perror("waitpid");
+            }
+        }
+    }
+
+    free(line);
+    return 0;
+}
---
+++ print.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("FAWEH\n");
+    return 0;
+}
